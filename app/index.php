6d20ff7621c72cddb3e3771ad1d3268a0b892a6a
Jana Hassoun <janahassoun391@gmail.com>
2026-05-19 11:37:49 -0400

    edit Jenkinsfile

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "password", "recommender");
+$conn = new mysqli("db", "root", "password", "recommender");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
